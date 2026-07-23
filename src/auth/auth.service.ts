20876f4f25caee3808d2a8ae062a7582fef77b00
sisiam08 <mohammadsisiam08@gmail.com>
2026-07-23 09:29:22 +0700

    feat(auth): implement user login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, ForbiddenException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user.entity';
+import { User, UserStatus } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 import { SignupDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+import { AuthResult } from './interfaces/auth.interface';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +15,12 @@ export class AuthService {
         private readonly userRepo: Repository<User>,
         private readonly configService: ConfigService,
     ) { }
+
+    private asserUserActive(status: UserStatus): void {
+        if (status === UserStatus.SUSPENDED) throw new ForbiddenException("Account is suspended");
+
+        if (status === UserStatus.BANNED) throw new ForbiddenException("Account is banned");
+    }
 
     async signup(payload: SignupDto): Promise<Pick<User, 'id' | 'fullName' | 'email' | 'status'>> {
         const { fullName, email, password } = payload;
@@ -42,6 +50,43 @@ export class AuthService {
             return restUser;
         } catch (error) {
             throw error
+        }
+    }
+
+    async login(payload: LoginDto): Promise<AuthResult> {
+        const { email, password } = payload;
+
+        try {
+            const user = await this.userRepo.findOne({
+                where: {
+                    email
+                },
+                select: {
+                    id: true,
+                    fullName: true,
+                    email: true,
+                    password: true,
+                    status: true
+                }
+            })
+            if (!user || !user.password) {
+                throw new UnauthorizedException("Invalid Credentials!")
+            }
+            const isPasswordValid = await bcrypt.compare(password, user.password!);
+
+            if (!isPasswordValid) {
+                throw new UnauthorizedException("Invalid Credentials!");
+            }
+
+            this.asserUserActive(user.status);
+
+            return {
+                user: { id: user.id, fullName: user.fullName, email: user.email },
+                accessToken: "",
+                refreshToken: ""
+            }
+        } catch (error) {
+            throw new InternalServerErrorException("Internal Server Error");
         }
     }
 }
