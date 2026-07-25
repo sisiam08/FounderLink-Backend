7565022a19777c252aff516a74e81893e4ffe283
Sahed Ahmed <asahed532@gmail.com>
2026-07-25 12:54:51 +0600

    Make Uploade Module

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from '../profile/entities/profile.entity';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Profile])],
   controllers: [UploadController],
   providers: [UploadService],
+  exports: [UploadService],
 })
 export class UploadModule {}
