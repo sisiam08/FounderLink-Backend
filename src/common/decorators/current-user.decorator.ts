64a6e62db3a6ff5bde30cae4066b2f8f361edc01
Taneem69 <tanvirtaneem32@gmail.com>
2026-07-26 21:52:56 +0600

    added current user decorator

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,11 +1,23 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import { AuthenticatedRequest, AuthenticatedUser } from "src/auth/interfaces/auth.interface";
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 
-export const CurrentUser = createParamDecorator((data: keyof AuthenticatedUser | undefined, context: ExecutionContext)=>{
-    const request = context.switchToHttp().getRequest<AuthenticatedRequest>();
+interface AuthenticatedRequest extends Request {
+  user?: {
+    userId: string;
+    sessionId: string;
+  };
+}
 
+export const CurrentUser = createParamDecorator(
+  (
+    data: keyof { userId: string; sessionId: string } | undefined,
+    ctx: ExecutionContext,
+  ) => {
+    const request = ctx.switchToHttp().getRequest<AuthenticatedRequest>();
     const user = request.user;
-    if(!user) return undefined;
-
-    return data ? user[data] : user;    
-})
+    if (!user) {
+      return undefined;
+    }
+    return data ? user[data] : user;
+  },
+);
