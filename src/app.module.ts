7c61b946517d232e42368a46897eedc859b1d211
Taneem69 <tanvirtaneem32@gmail.com>
2026-07-27 16:00:55 +0600

    fixed all the errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,42 @@
-import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { LoggingMiddleware } from './common/middleware/logging.middleware';
+import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
+import { MessageModule } from './message/message.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { NotificationModule } from './notification/notification.module';
 import { RequirementModule } from './requirement/requirement.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres' as const,
-        url: configService.get<string>('DATABASE_URL'),
+        type: 'postgres',
+        url: configService.getOrThrow<string>('DATABASE_URL'),
         autoLoadEntities: true,
-        synchronize: configService.get<string>('NODE_ENV') !== 'production',
-        logging: configService.get<string>('NODE_ENV') === 'development',
-      }),
+        synchronize: configService.getOrThrow<string>('NODE_ENV') !== 'production',
+        ssl: true,
+        retryAttempts: 3,
+        retryDelay: 3000,
+        connectTimeoutMS: 15000,
+      })
     }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
-      serveRoot: '/uploads',
-    }),
-    RequirementModule,
+    EventEmitterModule.forRoot(),
+    AuthModule,
+    MailModule,
+    MessageModule,
+    NotificationModule,
+    RequirementModule
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes('*');
-  }
-}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule);
+  
+  app.use(cookieParser());
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
 
-  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
-  app.enableCors();
-
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  new Logger('Bootstrap').log(`Application running on port ${port}`);
+  await app.listen(process.env.PORT ?? 5000);
 }
-
-void bootstrap();
+bootstrap();
