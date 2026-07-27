b51848a7c6cd1dd1ff561d74a1491d0226fa0fc9
Taneem69 <tanvirtaneem32@gmail.com>
2026-07-27 13:38:37 +0600

    fixed roles decorator

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
-import { SystemRole } from 'src/user/entities/user.entity';
+import { SystemRole } from '../../user/entities/user.entity';
 
 export const ROLE_KEY = 'roles';
 export const Roles = (...roles: SystemRole[]) => SetMetadata(ROLE_KEY, roles);
