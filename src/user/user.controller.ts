79c60bc44a3da027607f50fef3c206de8b664f77
sisiam08 <mohammadsisiam08@gmail.com>
2026-07-29 00:35:11 +0600

    feat(user): admin can get or revoke user sessions

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Get, Param, Patch, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Query } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { Roles } from "src/common/decorators/roles.decorator";
 import { SystemRole } from "./entities/user.entity";
 import { UpdateUserStatusDto } from "./dto/update-user-status.dto";
 import { UserListQueryDto } from "./dto/user-list-query.dto";
 import { ChangeUserRoleDto } from "./dto/change-user-role.dto";
+import { SessionService } from "src/auth/session.service";
 
 @Controller('users')
 @Roles(SystemRole.SUPER_ADMIN, SystemRole.ADMIN)
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  constructor(
+    private readonly userService: UserService,
+    private readonly sessionService: SessionService
+  ) { }
 
   @Get()
   async getAllUserList(
@@ -47,6 +51,25 @@ export class UserController {
     @Body() payload: ChangeUserRoleDto,
   ) {
     return this.userService.changeUserRole(id, payload.systemRole);
+  }
+
+   @Get(':id/sessions')
+  async getUserSessions(@Param('id') id: string) {
+    return this.sessionService.getUserSessions(id);
+  }
+
+  @Delete('sessions/:sessionId')
+  async revokeSession(
+    @Param('sessionId') sessionId: string,
+  ) {
+    return this.sessionService.revokeSession(sessionId);
+  }
+
+  @Delete(':id/sessions')
+  async revokeAllSessions(
+    @Param('id') id: string,
+  ) {
+    return this.sessionService.revokeAllSessions(id);
   }
 
 }
