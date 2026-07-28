321a4848763adc083cbd504cabb20317ff8716f9
PulockKundu <pulock7643@gmail.com>
2026-07-28 19:37:08 +0600

    Endpoint /startups created for create a startup idea

---
+++ src/startup/startup.service.ts
@@ -0,0 +1,45 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CofounderRequirement } from '../requirement/entities/cofounder-requirement.entity';
+import { User } from '../user/entities/user.entity';
+import { CreateRequirementDto } from '../requirement/dto/create-requirement.dto';
+import { CreateStartupDto } from './dto/create-startup.dto';
+import { UpdateStartupDto } from './dto/update-startup.dto';
+import { StartupIdea, StartupStatus } from './entities/startup-idea.entity';
+
+@Injectable()
+export class StartupService {
+  constructor(
+    @InjectRepository(StartupIdea)
+    private readonly startupRepo: Repository<StartupIdea>,
+
+    @InjectRepository(CofounderRequirement)
+    private readonly requirementRepo: Repository<CofounderRequirement>,
+  ) {}
+
+  async createStartup(
+    userId: string,
+    createStartupDto: CreateStartupDto,
+  ): Promise<StartupIdea> {
+    const startup = this.startupRepo.create({
+      owner: {
+        id: userId,
+      } as User,
+      title: createStartupDto.title,
+      shortDescription: createStartupDto.shortDescription,
+      fullDescription: createStartupDto.fullDescription,
+      industries: createStartupDto.industries,
+      startupStage: createStartupDto.startupStage,
+      status: StartupStatus.OPEN,
+    });
+
+    return await this.startupRepo.save(startup);
+  }
+}
+
