17a81ae2c501e533b33050a697e42347160cf389
Sahed Ahmed <asahed532@gmail.com>
2026-07-28 16:49:41 +0600

    update Common decorator

---
+++ src/auth/entities/interfaces/auth.interface.ts
@@ -0,0 +1,24 @@
+import { Request } from "express";
+import { SystemRole, User, UserStatus } from "src/user/entities/user.entity";
+
+export interface AuthResult {
+    user: Pick<User, 'id' | 'fullName' | 'email'>;
+    accessToken: string;
+    refreshToken: string;
+}
+
+export interface JwtPayload {
+    userId: string,
+    sessionId: string
+}
+
+export interface AuthenticatedUser {
+    userId: string;
+    sessionId: string;
+    systemRole: SystemRole,
+    status: UserStatus
+}
+
+export interface AuthenticatedRequest extends Request{
+    user: AuthenticatedUser
+}
