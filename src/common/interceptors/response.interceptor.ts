de5e48af515d1ce1a1164572f0679c301254393a
Sahed Ahmed <asahed532@gmail.com>
2026-07-28 21:08:14 +0600

    Update common

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -1,33 +1,23 @@
-import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-} from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { map, Observable } from "rxjs";
 
-export interface StandardResponse<T> {
-  success: boolean;
-  data: T;
-  timestamp: string;
+interface StandardResponse<T>{
+    success: boolean,
+    data: T,
+    timestamp: string
 }
 
 @Injectable()
-export class ResponseInterceptor<T> implements NestInterceptor<
-  T,
-  StandardResponse<T>
-> {
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler<T>,
-  ): Observable<StandardResponse<T>> {
-    return next.handle().pipe(
-      map((data) => ({
-        success: true,
-        data,
-        timestamp: new Date().toISOString(),
-      })),
-    );
-  }
+export class ResponseInterceptor<T> implements NestInterceptor<T, StandardResponse<T>>{
+    intercept(context: ExecutionContext, next: CallHandler<T>): Observable<StandardResponse<T>> | Promise<Observable<StandardResponse<T>>> {
+        return next.handle().pipe(
+            map((data)=>{
+                return {
+                    success: true,
+                    data,
+                    timestamp: new Date().toISOString()
+                }
+            })
+        )
+    }
 }
