f8f1c4b788127bbbbcdb7b9453f391175b504aaf
Tanvir Mahtab Taneem <144769980+Taneem69@users.noreply.github.com>
2026-07-28 22:30:21 +0600

    Fixed requrirement controller

--- src/requirement/requirement.controller.ts
+++ src/requirement/requirement.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Post,
   Param,
   Patch,
   Query,
@@ -12,17 +13,18 @@ import { UpdateRequirementDto } from './dto/update-requirement.dto';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { SystemRole } from 'src/user/entities/user.entity';
+import { CreateRequirementDto } from './dto/create-requirement.dto';
 
 @Controller('requirements')
 export class RequirementController {
   constructor(private readonly requirementService: RequirementService) {}
 
+  @Post('startup/:ideaId')
+  async create(@Param('ideaId') ideaId: string,@Body() dto: CreateRequirementDto,@CurrentUser('userId') userId: string,) {
+    return this.requirementService.createRequirement(ideaId, userId, dto);
+  }
   @Patch(':id')
-  async update(
-    @Param('id') id: string,
-    @Body() dto: UpdateRequirementDto,
-    @CurrentUser('userId') userId: string,
-  ) {
+  async update(@Param('id') id: string,@Body() dto: UpdateRequirementDto,@CurrentUser('userId') userId: string,) {
     return this.requirementService.updateRequirement(id, userId, dto);
   }
 
@@ -38,13 +40,7 @@ export class RequirementController {
   }
 
   @Get('browse')
-  async browse(
-    @CurrentUser('userId') userId: string,
-    @Query('cursor') cursor?: { createdAt: string; id: string },
-    @Query('role') role?: string,
-    @Query('industry') industry?: string,
-    @Query('stage') stage?: string,
-  ) {
+  async browse(@CurrentUser('userId') userId: string,@Query('cursor') cursor?: { createdAt: string; id: string },@Query('role') role?: string,@Query('industry') industry?: string,@Query('stage') stage?: string,) {
     return this.requirementService.browseRequirements(
       userId,
       cursor,
@@ -55,21 +51,13 @@ export class RequirementController {
   }
 
   @Get(':id')
-  async getById(
-    @Param('id') id: string,
-    @CurrentUser('userId') userId: string,
-  ) {
+  async getById(@Param('id') id: string,@CurrentUser('userId') userId: string,) {
     return this.requirementService.getRequirementById(id, userId);
   }
 
   @Get('requirements')
   @Roles(SystemRole.ADMIN, SystemRole.SUPER_ADMIN)
-  async listRequirements(
-    @Query('status') status?: string,
-    @Query('role') role?: string,
-    @Query('page') page?: string,
-    @Query('limit') limit?: string,
-  ) {
+  async listRequirements(@Query('status') status?: string,@Query('role') role?: string,@Query('page') page?: string,@Query('limit') limit?: string) {
     return this.requirementService.listRequirements(
       status,
       role,
@@ -80,10 +68,7 @@ export class RequirementController {
 
   @Patch('requirements/:id/close')
   @Roles(SystemRole.ADMIN, SystemRole.SUPER_ADMIN)
-  async forceCloseRequirement(
-    @Param('id') id: string,
-    @CurrentUser('userId') adminId: string,
-  ) {
+  async forceCloseRequirement(@Param('id') id: string,@CurrentUser('userId') adminId: string) {
     return this.requirementService.forceCloseRequirement(id, adminId);
   }
 }
